4877b2b21deebc62e49c7655c12f89e9b6e7dfb4
ohysh <ohyshka@gmail.com>
2025-05-21 00:49:19 +0300

    added labs

--- src/lab5/Task1.java
+++ src/lab5/Task1.java
@@ -1,23 +1,6 @@
 package lab5; // Оголошуємо пакет lab5, в якому знаходяться всі файли
 
-//// Task1.java - Обчислення площі прямокутника
-//class Task1 {
-//    private int width;
-//    private int height;
-//
-//    public Task1(int width, int height) {
-//        this.width = width;
-//        this.height = height;
-//    }
-//
-//    public void run() {
-//        System.out.println("Площа прямокутника: " + calculateArea());
-//    }
-//
-//    public int calculateArea() {
-//        return width * height;
-//    }
-//}
+
 
 // Task1.java - Обчислення площі прямокутника
 class Task1 {
